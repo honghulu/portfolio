ac53903ac1bad867cf4fe9fe89a95379a4339114
honghulu <honghulu.ai@gmail.com>
2023-12-01 01:18:39 -0800

    bug fix: react/no-unescaped-entities

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,6 +10,7 @@ function HeroSection(props) {
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                            {/* eslint-disable-next-line react/no-unescaped-entities */}
                             Hello, I'm{" "}
                         </span>
                         <br/>
