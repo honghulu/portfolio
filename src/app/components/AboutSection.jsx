f32a284790cadd6d14a77a06e68f566299f1d2f6
honghulu <honghulu.ai@gmail.com>
2023-12-01 18:03:42 -0800

    add detail tabs for about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,44 @@ import React, { useTransition, useState } from 'react';
 import Image from "next/image";
 import TabButton from "@/app/components/TabButton";
 
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>PostgreSQL</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Git</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>MS in Computer Science, University of Southern California</li>
+                <li>BS in Computer Science, minor in Japanese, University of Arizona</li>
+            </ul>
+        )
+    },
+    {
+        title: "Experience",
+        id: "experience",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>HuupAI, Backend Software and Machine Learning Engineer Intern</li>
+                <li>Amazon, Software Development Engineer Intern</li>
+                <li>College of Pharmacy @ UA, Undergraduate Research Assistant</li>
+            </ul>
+        )
+    },
+]
+
 const AboutSection = () => {
     const [tab, setTab] = useState("skills");
     const [isPending, startTransition] = useTransition();
@@ -15,9 +53,9 @@ const AboutSection = () => {
 
     return (
         <section className="text-white">
-            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about-image.png" width={500} height={500}  alt="about me image"/>
-                <div>
+            <div className="md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+                <Image className="md:mt-16" src="/images/about-image.png" width={500} height={500}  alt="about me image"/>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
                         I am a recent graduated MSCS student at USC. I have a strong cs background.
@@ -28,7 +66,7 @@ const AboutSection = () => {
                         including CI/CD, Lambda API, Databases (Redshift, DynamoDB), etc.
                         And I am always curious and a fast learner.
                     </p>
-                    <div className="flex flex-row mt-8">
+                    <div className="flex flex-row justify-start mt-8">
                         <TabButton
                             selectTab={() => handleTabChange("skills")}
                             active={tab === "skills"}
@@ -50,6 +88,9 @@ const AboutSection = () => {
                             {" "}
                             Experience{" "}
                         </TabButton>
+                    </div>
+                    <div className="mt-8">
+                        {TAB_DATA.find((t) => t.id === tab).content}
                     </div>
                 </div>
             </div>
