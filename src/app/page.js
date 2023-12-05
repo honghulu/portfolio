b2b33591954fed277a69d4431ca6a3bde283614b
honghulu <honghulu.ai@gmail.com>
2023-12-05 00:35:43 -0800

    add project section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import HeroSection from "@/app/components/HeroSection";
 import Navbar from "@/app/components/Navbar";
 import AboutSection from "@/app/components/AboutSection";
+import ProjectsSection from "@/app/components/ProjectsSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
           <HeroSection/>
           <AboutSection/>
+          <ProjectsSection/>
       </div>
     </main>
   )
