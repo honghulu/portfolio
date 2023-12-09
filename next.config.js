1fef437e03ad9b71415a58a898cbcffa1f17f0c0
honghulu <honghulu.ai@gmail.com>
2023-12-08 17:51:15 -0800

    add output export

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     basePath: isProd ? '/portfolio' : '',
+    output: 'export',
     distDir: 'dist',
     images: {
         unoptimized: true,
