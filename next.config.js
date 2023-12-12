f6934af0ca470f824c96203e664142a90de00bb8
honghulu <honghulu.ai@gmail.com>
2023-12-12 02:37:20 -0800

    no base dir

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/portfolio' : '',
+    basePath: isProd ? '' : '',
     output: 'export',
     distDir: 'dist',
     images: {
