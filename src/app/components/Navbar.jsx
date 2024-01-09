7e2fb85c966ca0c4f5fb518fdaea9140e8c67bb2
honghulu <honghulu.ai@gmail.com>
2024-01-08 23:59:13 -0800

    add portfolio

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import NavLink from "@/app/components/NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "@/app/components/MenuOverlay";
+import Image from "next/image";
 
 const navLinks = [
     {
@@ -27,7 +28,7 @@ const Navbar = () => {
         <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-                    LOGO
+                    <Image className="" src={`/images/portfolio-honghu-logo.png`} width={132} height={60} alt="navbar logo"/>
                 </Link>
                 <div className="mobile-menu block md:hidden">
                     {
